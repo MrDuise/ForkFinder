248d8f31e8b02d752122c8fde1d87f611a84ac3c
Michael Duisenberg <mrduise@gmail.com>
2025-09-03 19:36:57 -0700

    finished create session function
    finished create session function

--- src/modules/session/session.module.ts
+++ src/modules/session/session.module.ts
@@ -5,6 +5,7 @@ import { SessionService } from './services/session.service';
 import { SessionController } from './session.controller';
 import { RedisConfigModule } from '../redis/redis.module';
 import { AuthModule } from '../auth/auth.module';
+import { GoogleMapsService } from 'src/restaurant-search/google-maps.service';
 
 @Module({
   imports: [
@@ -12,7 +13,7 @@ import { AuthModule } from '../auth/auth.module';
     AuthModule,
     RedisConfigModule,
   ],
-  providers: [SessionService],
+  providers: [SessionService, GoogleMapsService],
   controllers: [SessionController],
   exports: [SessionService],
 })
